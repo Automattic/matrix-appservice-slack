da8ff5d555064ac749a09b89815b05dabd5b78f4
Paulo Pinto <paulo.pinto@automattic.com>
2023-07-06 12:32:34 +0100

    Add getBySlackUserId()

--- src/MatrixUsernameStore.ts
+++ src/MatrixUsernameStore.ts
@@ -1,10 +1,21 @@
 import {Datastore} from "./datastore/Models";
 import {IConfig} from "./IConfig";
 
+type MatrixUsername = string;
+
 export class MatrixUsernameStore {
     constructor(
         private datastore: Datastore,
         private config: IConfig,
     ) {
+    }
+
+    async getBySlackUserId(slackUserId: string): Promise<MatrixUsername | null> {
+        const username = await this.datastore.getMatrixUsername(slackUserId);
+        if (!username) {
+            return null;
+        }
+
+        return `@${username}:${this.config.homeserver.server_name}`;
     }
 }
