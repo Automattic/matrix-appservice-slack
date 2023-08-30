fa37df0f34847888fa773854ae11160363e24716
Paulo Pinto <paulo.pinto@automattic.com>
2023-08-30 12:11:57 +0100

    Reorder methods

--- src/SlackMessageParser.ts
+++ src/SlackMessageParser.ts
@@ -76,6 +76,49 @@ export class SlackMessageParser {
         return parsedMessage;
     }
 
+    private parseText(text: string): TextualMessageEventContent {
+        // TODO: This is fixing plaintext mentions, but should be refactored.
+        // https://github.com/matrix-org/matrix-appservice-slack/issues/110
+        const body = text.replace(/<https:\/\/matrix\.to\/#\/@.+:.+\|(.+)>/g, "$1");
+
+        // TODO: Slack's markdown is their own thing that isn't really markdown,
+        // but the only parser we have for it is slackdown. However, Matrix expects
+        // a variant of markdown that is in the realm of sanity. Currently text
+        // will be slack's markdown until we've got a slack -> markdown parser.
+        let formattedBody: string = Slackdown.parse(text);
+
+        // Parse blockquotes.
+        const blocks: string[] = [];
+        let currentQuote = "";
+        const quoteDelimiter = "> ";
+        for (const line of formattedBody.split("\n")) {
+            if (line.startsWith(quoteDelimiter)) {
+                currentQuote += line.replace(quoteDelimiter, "") + "<br>";
+            } else {
+                if (currentQuote !== "") {
+                    blocks.push(`<blockquote>${currentQuote}</blockquote>`);
+                }
+                blocks.push(`${line}<br>`);
+                currentQuote = "";
+            }
+        }
+        if (currentQuote !== "") {
+            blocks.push(`<blockquote>${currentQuote}</blockquote>`);
+        }
+
+        if (blocks.length > 0) {
+            formattedBody = blocks.join("");
+        }
+        formattedBody = formattedBody.replace("\n", "<br>");
+
+        return {
+            msgtype: "m.text",
+            format: "org.matrix.custom.html",
+            body,
+            formatted_body: formattedBody,
+        };
+    }
+
     private parseEdit(
         parsedMessage: TextualMessageEventContent,
         parsedPreviousMessage: TextualMessageEventContent,
@@ -118,49 +161,6 @@ export class SlackMessageParser {
                 body: newBody,
                 formatted_body: newFormattedBody,
             }
-        };
-    }
-
-    private parseText(text: string): TextualMessageEventContent {
-        // TODO: This is fixing plaintext mentions, but should be refactored.
-        // https://github.com/matrix-org/matrix-appservice-slack/issues/110
-        const body = text.replace(/<https:\/\/matrix\.to\/#\/@.+:.+\|(.+)>/g, "$1");
-
-        // TODO: Slack's markdown is their own thing that isn't really markdown,
-        // but the only parser we have for it is slackdown. However, Matrix expects
-        // a variant of markdown that is in the realm of sanity. Currently text
-        // will be slack's markdown until we've got a slack -> markdown parser.
-        let formattedBody: string = Slackdown.parse(text);
-
-        // Parse blockquotes.
-        const blocks: string[] = [];
-        let currentQuote = "";
-        const quoteDelimiter = "> ";
-        for (const line of formattedBody.split("\n")) {
-            if (line.startsWith(quoteDelimiter)) {
-                currentQuote += line.replace(quoteDelimiter, "") + "<br>";
-            } else {
-                if (currentQuote !== "") {
-                    blocks.push(`<blockquote>${currentQuote}</blockquote>`);
-                }
-                blocks.push(`${line}<br>`);
-                currentQuote = "";
-            }
-        }
-        if (currentQuote !== "") {
-            blocks.push(`<blockquote>${currentQuote}</blockquote>`);
-        }
-
-        if (blocks.length > 0) {
-            formattedBody = blocks.join("");
-        }
-        formattedBody = formattedBody.replace("\n", "<br>");
-
-        return {
-            msgtype: "m.text",
-            format: "org.matrix.custom.html",
-            body,
-            formatted_body: formattedBody,
         };
     }
 
@@ -259,5 +259,4 @@ export class SlackMessageParser {
         }
         return channel;
     }
-
 }
