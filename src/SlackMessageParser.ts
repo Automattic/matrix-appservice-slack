779986caa927b482fecb4031f110787ce7ad0156
Paulo Pinto <paulo.pinto@automattic.com>
2023-08-30 14:12:44 +0100

    Call replaceChannelIdsWithNames()

--- src/SlackMessageParser.ts
+++ src/SlackMessageParser.ts
@@ -69,18 +69,20 @@ export class SlackMessageParser {
         const teamDomain = await this.main.getTeamDomainForMessage(message);
 
         const file = subtype === "file_comment" ? message.file : undefined;
-        const parsedMessage = await this.doParse(text, teamDomain, file);
+        const parsedMessage = await this.doParse(text, slackClient, teamDomain, file);
 
         if (subtype === "message_changed" && message.previous_message?.text) {
-            const parsedPreviousMessage = await this.doParse(message.previous_message.text, teamDomain);
+            const parsedPreviousMessage = await this.doParse(message.previous_message.text, slackClient, teamDomain);
             return this.parseEdit(parsedMessage, parsedPreviousMessage, replyEvent);
         }
 
         return parsedMessage;
     }
 
-    private async doParse(text: string, teamDomain?: string, file?: ISlackFile): Promise<TextualMessageEventContent> {
-        let body = text.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g, "&");
+    private async doParse(body: string, slackClient: WebClient, teamDomain?: string, file?: ISlackFile): Promise<TextualMessageEventContent> {
+        body = await this.replaceChannelIdsWithNames(body, slackClient);
+
+        body = body.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g, "&");
         body = body.replace("<!channel>", "@room");
         body = body.replace("<!here>", "@room");
         body = body.replace("<!everyone>", "@room");
@@ -201,7 +203,7 @@ export class SlackMessageParser {
         return `> <${replyEvent.sender}> ${originalBody.split("\n").join("\n> ")}`;
     }
 
-    private async replaceChannelIdsWithNames(message: ISlackMessageEvent, text: string, slackClient: WebClient): Promise<string> {
+    private async replaceChannelIdsWithNames(text: string, slackClient: WebClient): Promise<string> {
         let match: RegExpExecArray | null = null;
         while ((match = CHANNEL_ID_REGEX.exec(text)) !== null) {
             // foreach channelId, pull out the ID
